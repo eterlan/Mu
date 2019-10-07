961932a82480437b29f54fd0b5b75289284f6355
eterlan <703246769@qq.com>
2019-10-07 15:28:45 +0800

    Initial commit

---
+++ Temp/PlannerAssembly/AI.Planner.Domains/ConditionalAssembly.cs
@@ -0,0 +1,8 @@
+[assembly: ConditionalCompilation.OptionalDependency("AI.Planner.Domains.ConditionalType", "PLANNER_DOMAIN_GENERATED")]
+
+namespace AI.Planner.Domains
+{
+    public class ConditionalType
+    {
+    }
+}
